4cca50d88ad30aad75319feccc69492de24ed819
Include caleb.fun <oasis.mystre@gmail.com>
2025-05-11 18:08:49 +0100

    wip

--- src/bot/utils/private-func.ts
+++ src/bot/utils/private-func.ts
@@ -1,7 +1,8 @@
-import { Context } from "telegraf";
+import  type { Context } from "telegraf";
 
 export const privateFunc =
   <T extends Context, Fn extends (context: T) => Promise<unknown>>(fn: Fn) =>
   (context: T) => {
     if (context.chat?.type === "private") return fn(context);
+    else console.log("[channel.message] from=", context.chat?.id);
   };
