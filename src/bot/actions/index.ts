0d947d163bf6e07ff26cdbc0cab800b9b8a4212b
Include caleb.fun <oasis.mystre@gmail.com>
2025-05-16 09:14:00 +0100

    feat: bug fixes

--- src/bot/actions/index.ts
+++ src/bot/actions/index.ts
@@ -2,14 +2,16 @@ import type { Telegraf } from "telegraf";
 
 import onJoinAction from "./on-join-action";
 import onStartAction from "./on-start-action";
-import sendLinkAction from "./send-link-action";
 import scheduleAction from "./schedule-action";
+import sendLinkAction from "./send-link-action";
+import { webinarAction } from "./webinar-action";
 import rescheduleAction from "./reschedule-action";
 import setScheduleTimeAction from "./set-schedule-date-action";
 import setScheduleDateAction from "./set-schedule-time-action";
 
 export default function registerActions(bot: Telegraf) {
   onJoinAction(bot);
+  webinarAction(bot);
   onStartAction(bot);
   sendLinkAction(bot);
   scheduleAction(bot);
