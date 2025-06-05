a915702400014172f045fbb99c3d8109de941c6a
Include caleb.fun <oasis.mystre@gmail.com>
2025-06-05 10:09:39 +0100

    feat: wip

--- drizzle/0002_familiar_starbolt.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "messages" ALTER COLUMN "media" SET DATA TYPE jsonb;--> statement-breakpoint
-ALTER TABLE "messages" ALTER COLUMN "buttons" SET DATA TYPE jsonb[];--> statement-breakpoint
-ALTER TABLE "messages" ADD COLUMN "stats" jsonb;
