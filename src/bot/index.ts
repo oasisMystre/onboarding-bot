6485a52950fe9eb5ac089cfea6e59a5502e54bb2
Include caleb.fun <oasis.mystre@gmail.com>
2025-06-11 21:19:24 +0100

    feat: wip

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -1,12 +1,30 @@
-import { session, type Telegraf } from "telegraf";
+import { parse } from "pg-connection-string";
+import { Postgres } from "@telegraf/session/pg";
+import { session, SessionStore, type Telegraf } from "telegraf";
 
 import { stage } from "./scenes";
+import { getEnv } from "../env";
+import { SessionData } from "../global";
 import registerActions from "./actions";
 import registerCommands from "./commands";
 import { authenticateUser } from "./middlewares/authenticate-user";
 
 export default function registerBot(bot: Telegraf) {
-  bot.use(session());
+  const config = parse(getEnv("DATABASE_URL"));
+  const store: SessionStore<SessionData> = Postgres({
+    user: config.user!,
+    host: config.host!,
+    password: config.password!,
+    database: config.database!,
+    config: {
+      ssl: config.ssl as boolean,
+    },
+  });
+  bot.use(
+    session({
+      store,
+    })
+  );
   bot.use(stage.middleware());
   bot.use(authenticateUser);
 
