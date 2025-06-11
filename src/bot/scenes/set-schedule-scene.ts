7f85fd4a6ea7ee8310d26341bf981f9ba0a7f4dc
Include caleb.fun <oasis.mystre@gmail.com>
2025-06-11 21:11:32 +0100

    wip

--- src/bot/scenes/set-schedule-scene.ts
+++ src/bot/scenes/set-schedule-scene.ts
@@ -3,6 +3,7 @@ import { readFileSync } from "fs";
 import { Markup, Scenes } from "telegraf";
 
 import { db } from "../../instances";
+import { format } from "../../utils/format";
 import { updateMessageById } from "../../controllers/message.controller";
 import { buildBroadcastMessage } from "./broadcast-scene/build-message";
 
@@ -41,34 +42,49 @@ export const setScheduleScene = new Scenes.WizardScene(
         : undefined;
 
     if (text) {
-      const schedule = moment(
-        text,
-        [
-          "D/M/YYYY h:mm A",
-          "D/M/YYYY h:mm",
-          "D/M/YYYY",
-          "DD/MM/YYYY h:mm A",
-          "DD/MM/YYYY h:mm",
-          "DD/MM/YYYY",
-        ],
-        false
-      );
-      const [message] = await updateMessageById(
-        db,
-        context.session.broadcast.id,
-        { schedule: schedule.toDate() }
-      );
+      try {
+        const schedule = moment(
+          text,
+          [
+            "D/M/YYYY h:mm A",
+            "D/M/YYYY h:mm",
+            "D/M/YYYY",
+            "DD/MM/YYYY h:mm A",
+            "DD/MM/YYYY h:mm",
+            "DD/MM/YYYY",
+          ],
+          false
+        );
+        const [message] = await updateMessageById(
+          db,
+          context.session.broadcast.id,
+          { schedule: schedule.toDate() }
+        );
 
-      if (message)
-        return Promise.allSettled([
-          context.scene.leave(),
-          context.session.broadcast.messageId
-            ? context
-                .deleteMessages([context.session.broadcast.messageId])
-                .then(() => (context.session.broadcast.messageId = undefined))
-            : undefined,
-          buildBroadcastMessage(context, message),
-        ]);
+        if (message)
+          return Promise.allSettled([
+            context.scene.leave(),
+            context.session.broadcast.messageId
+              ? context
+                  .deleteMessages([context.session.broadcast.messageId])
+                  .then(() => (context.session.broadcast.messageId = undefined))
+              : undefined,
+            buildBroadcastMessage(context, message),
+          ]);
+      } catch {
+        return context.replyWithMarkdownV2(
+          format(
+            "*Invalid Date\\.*\n %",
+            readFileSync("locale/en/tools/broadcast/schedule.md", "utf-8")
+          ),
+          {
+            link_preview_options: {
+              is_disabled: true,
+            },
+            reply_markup: Markup.forceReply().reply_markup,
+          }
+        );
+      }
     }
   }
 );
